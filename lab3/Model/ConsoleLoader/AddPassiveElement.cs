05c333a51ccb760e8d9f7b44085f71fbfbf87d70
KoptsevIO <ilia.koptsev_mail.com@mail.ru>
2023-04-01 22:42:30 +0700

    feature(AddPassiveElement): добавлены новые фичи
    - метод добавления параметров пассивным элементам AddElement()
    - метод использования Action ActionHandler()

--- lab3/Model/ConsoleLoader/AddPassiveElement.cs
+++ lab3/Model/ConsoleLoader/AddPassiveElement.cs
@@ -1,4 +1,5 @@
 ﻿using PassiveElement;
+using System.Numerics;
 
 namespace ConsoleLoader
 {
@@ -29,6 +30,155 @@ namespace ConsoleLoader
             }
 
             return checkNumber;
+        }
+
+        /// <summary>
+        /// Метод добавления параметров пассивным элементам.
+        /// </summary>
+        /// <exception cref="ArgumentException">ArgumentException.
+        /// </exception>
+        public static void AddElement()
+        {
+            PassiveElementBase element = new Resistor();
+
+            Action actionStart = new Action(() =>
+            {
+                Console.Write($"1 - резистор,\n" +
+                    $"2 - конденсатор,\n3 - катушка индуктивности." +
+                    $"\nРасчёт сопротивления пассивного элемента:");
+
+                int who = int.Parse(Console.ReadLine());
+
+                switch (who)
+                {
+                    case 1:
+                        {
+                            element = new Resistor();
+                            break;
+                        }
+
+                    case 2:
+                        {
+                            element = new Condenser();
+                            break;
+                        }
+                    case 3:
+                        {
+                            element = new Inductor();
+                            break;
+                        }
+                    default:
+                        {
+                            throw new ArgumentException("Введите " +
+                                "корректно.");
+                            break;
+                        }
+                }
+            });
+
+            var actionResistor = new List<(Action, string)>
+            {
+                (new Action(() =>
+                {
+                    Console.Write("Введите сопротивление резистора, Ом: ");
+                    Resistor resistor = (Resistor)element;
+
+                    resistor.Resistance = CheckNumber(Console.ReadLine());
+
+                }), "резистор"),
+                (new Action(() =>
+                {
+                    Resistor resistor = (Resistor)element;
+                    Console.WriteLine("Комплексное сопротивление " +
+                        "резистора, Ом: " + resistor.CalculationImpedance()
+                        + "\n");
+                    _ = Console.ReadKey();
+                }), "резистор")
+            };
+
+            var actionCondenser = new List<(Action, string)>
+            {
+                (new Action(() =>
+                {
+                    Condenser condenser = (Condenser)element;
+                    Console.Write("Введите ёмкость конденсатора, мкФ: ");
+
+                    condenser.Capacity = CheckNumber(Console.ReadLine());
+
+                }), "конденсатор"),
+                (new Action(() =>
+                {
+                    Console.WriteLine($"Комплексное сопротивление " +
+                        $"конденсатора, Ом: " +
+                        $"{element.CalculationImpedance()}" + "\n");
+                    _ = Console.ReadKey();
+
+                }), "конденсатор")
+            };
+
+            var actionInductor = new List<(Action, string)>
+            {
+                (new Action(() =>
+                {
+                    Inductor inductor = (Inductor)element;
+                    Console.Write("Введите индуктивность катушки" +
+                        " индуктивности, мГн: ");
+
+                    inductor.Inductance = CheckNumber(Console.ReadLine());
+
+                }), "катушка индуктивности"),
+                (new Action(() =>
+                {
+                    Console.WriteLine($"Комплексное сопротивление" +
+                        $"катушки индуктивности, Ом: " +
+                        $"{element.CalculationImpedance()}" + "\n");
+                    _ = Console.ReadKey();
+
+                }), "катушка индуктивности")
+            };
+
+            // Выбор пассивного элемента
+            ActionHandler(actionStart, "Пассивный элемент");
+
+            var passiveElementActionDictionary = new Dictionary<Type, 
+                List<(Action, string)>>
+            {
+                {typeof(Resistor), actionResistor},
+                {typeof(Condenser), actionCondenser},
+                {typeof(Inductor), actionInductor},
+            };
+
+            foreach (var action in passiveElementActionDictionary
+                [element.GetType()])
+            {
+                ActionHandler(action.Item1, action.Item2);
+            }
+        }
+
+        /// <summary>
+        /// Метод использования Action.
+        /// </summary>
+        /// <param name="action">action.</param>
+        /// <param name="propertyName">propertyName.</param>
+        private static void ActionHandler(Action action, string 
+            propertyName)
+        {
+
+            while (true)
+            {
+                try
+                {
+                    action.Invoke();
+                    return;
+                }
+                catch (ArgumentException exception)
+                {
+
+                    Console.WriteLine($"Incorrect {propertyName}. " +
+                            $"Error: {exception.Message}");
+
+                }
+            }
         }
     }
 }
