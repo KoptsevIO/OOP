8224f3ec3191d3414752046b0223719d50c100b3
KoptsevIO <ilia.koptsev_mail.com@mail.ru>
2023-04-01 23:02:59 +0700

    feature(ConsoleLoader): добавлены новые фичи
    - добавлен запуск программы из Program
    - исправлен выводимый в консоль текст

--- lab3/Model/ConsoleLoader/Program.cs
+++ lab3/Model/ConsoleLoader/Program.cs
@@ -1,13 +1,46 @@
 ﻿namespace ConsoleLoader
 {
+    /// <summary>
+    /// Класс Program.
+    /// </summary>
     public class Program
     {
+        /// <summary>
+        /// Основная программа.
+        /// </summary>
         private static void Main()
         {
             Console.WriteLine("Программа расчёта сопротивления " +
                 "пассивных элементов");
 
+            while (true)
+            {
 
+                Console.Write("\nРасчитать комплексное сопротивление " +
+                    "пассивных элементов - введите 1.\n" +
+                    "Закончить выполнение программы - введите 2." +
+                    "\nВвод: ");
+                bool result = short.TryParse(Console.ReadLine(),
+                            out short action);
+                if (result != true)
+                {
+                    Console.WriteLine("Введите число 1 или 2!");
+                }
+
+                if (action == 1)
+                {
+                    AddPassiveElement.AddElement();
+                }
+                else if (action == 2)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Не распознана команда. \n" +
+                        "Ведите заново.");
+                }
+            }
         }
     }
 }
