d634a27c6cfddd95f96d2041b24909cb8b48ef0c
KoptsevIO <ilia.koptsev_mail.com@mail.ru>
2023-05-09 21:11:38 +0700

    feature(View): создан новый класс событий ElementEventArgs

---
+++ lab4/Model/View/ElementEventArgs.cs
@@ -0,0 +1,24 @@
+﻿using PassiveElement;
+
+namespace View
+{
+    /// <summary>
+    /// Класс событий.
+    /// </summary>
+    public class ElementEventArgs : EventArgs
+    {
+        /// <summary>
+        /// Класс пассивных элементов.
+        /// </summary>
+        public PassiveElementBase Element { get; private set; }
+        
+        /// <summary>
+        /// Вызов элементов.
+        /// </summary>
+        /// <param name="element">Элемент.</param>
+        public ElementEventArgs(PassiveElementBase element)
+        {
+            Element = element;
+        }
+    }
+}
