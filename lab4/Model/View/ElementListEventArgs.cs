b0c11c6491226ac68779e052c080c5587b36a38d
KoptsevIO <ilia.koptsev_mail.com@mail.ru>
2023-05-09 21:17:25 +0700

    feature(View): создан класс список элементов ElementListEventArgs

---
+++ lab4/Model/View/ElementListEventArgs.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using PassiveElement;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace View
+{
+    /// <summary>
+    /// Класс событий.
+    /// </summary>
+    public class ElementListEventArgs : EventArgs
+    {
+        /// <summary>
+        /// Свойство список элементов.
+        /// </summary>
+        public BindingList<PassiveElementBase> ElementList { get; set; }
+
+        /// <summary>
+        /// Список элементов.
+        /// </summary>
+        /// <param name="element">Элемент.</param>
+        public ElementListEventArgs(BindingList<PassiveElementBase> element)
+        {
+            ElementList = element;
+        }
+
+    }
+}
