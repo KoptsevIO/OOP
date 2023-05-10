2ea27a21faf2c5e023f8576b3e3719539802131c
KoptsevIO <ilia.koptsev_mail.com@mail.ru>
2023-05-10 16:08:04 +0700

    feature(View): создан класс-форма для добавления параметров индуктивности AddInductorUserControl

---
+++ lab4/Model/View/AddInductorUserControl.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace View
+{
+    partial class AddInductorUserControl
+    {
+        /// <summary> 
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        /// <summary> 
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            Inductance = new TextBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(14, 34);
+            label1.Name = "label1";
+            label1.Size = new Size(113, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Индуктивность";
+            label1.Click += label1_Click;
+            // 
+            // Inductance
+            // 
+            Inductance.Location = new Point(234, 33);
+            Inductance.Name = "Inductance";
+            Inductance.Size = new Size(147, 27);
+            Inductance.TabIndex = 1;
+            Inductance.TextChanged += Inductance_TextChanged;
+            // 
+            // AddInductorUserControl
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(Inductance);
+            Controls.Add(label1);
+            Name = "AddInductorUserControl";
+            Size = new Size(402, 89);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox Inductance;
+    }
+}
---
+++ lab4/Model/View/AddInductorUserControl.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using PassiveElement;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace View
+{
+    /// <summary>
+    /// Класс параметров катушки индуктивности.
+    /// </summary>
+    public partial class AddInductorUserControl : UserControl
+    {
+        /// <summary>
+        /// Добавление параметров катушки индуктивности.
+        /// </summary>
+        public AddInductorUserControl()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Inductance_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
