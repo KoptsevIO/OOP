3b1f6368fdd707043c7ae86a5df7a4e1f0b3a115
KoptsevIO <ilia.koptsev_mail.com@mail.ru>
2023-05-10 15:48:00 +0700

    feature(View): добавлен новый класс-форма добавление параметров резистора AddResistorUserControl

---
+++ lab4/Model/View/AddResistorUserControl.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace View
+{
+    partial class AddResistorUserControl
+    {
+        /// <summary> 
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        /// <summary> 
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            Resistance = new TextBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(14, 34);
+            label1.Name = "label1";
+            label1.Size = new Size(187, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Активное сопротивление";
+            label1.Click += label1_Click;
+            // 
+            // Resistance
+            // 
+            Resistance.Location = new Point(234, 33);
+            Resistance.Name = "Resistance";
+            Resistance.Size = new Size(147, 27);
+            Resistance.TabIndex = 1;
+            Resistance.TextChanged += Resistance_TextChanged;
+            // 
+            // AddResistorUserControl
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(Resistance);
+            Controls.Add(label1);
+            Name = "AddResistorUserControl";
+            Size = new Size(402, 89);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox Resistance;
+    }
+}
---
+++ lab4/Model/View/AddResistorUserControl.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using PassiveElement;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace View
+{
+    /// <summary>
+    /// Добавление параметров резистора на форму AddResistorUserControl.
+    /// </summary>
+    public partial class AddResistorUserControl : UserControl, IAddElement
+    {
+        /// <summary>
+        /// Добавление параллелепипеда.
+        /// </summary>
+        public AddResistorUserControl()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        { }
+
+        private void Resistance_TextChanged(object sender, EventArgs e)
+        { }
+    }
+}
